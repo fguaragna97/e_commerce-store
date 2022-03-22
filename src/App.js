1f5ea1d37a0fdc2e476ae32625c9d3b602e45f94
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-03-21 17:58:01 -0700

    solve product and products error

--- src/App.js
+++ src/App.js
@@ -4,24 +4,22 @@ import { Products, Navbar } from "./components";
 
 const App = () => {
   // we create are products and how are we gonna set them
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
 
   // here we bring the products from our commerce library that its in charge of the whole backend
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
-
     setProducts(data);
   };
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  });
 
-  console.log(products);
   return (
     <div>
       <Navbar></Navbar>
-      <Products />
+      <Products products={products} />
     </div>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,32 +5,18 @@ import { Description } from "@material-ui/icons"
 import Product from "./Product/Product"
 import useStyles from "./styles"
 
-const products = [
-  {
-    id:1,
-    name:"Shoes",
-    description:"Running Shoes",
-    price:"$5",
-    image:""
-  },
-  {
-    id:2,
-    name:"Macbook",
-    description:"Apple macbook",
-    price:"$5",
-    image:""
-  }
-]
-
 // here we take how all the products gonna look we use the grid system of material ui
 
-const Products = () => {
+const Products = ({products}) => {
   const classes = useStyles()
+
+  console.log(products)
+
   return (
 <main className={classes.content}>
   <div className={classes.toolbar}></div>
    <Grid container justify="center" spacing={4}>
-        {products.map((product)=>(
+        {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={product}/>
           </Grid>
